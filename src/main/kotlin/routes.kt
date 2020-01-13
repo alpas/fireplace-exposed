d12e0023a946e7dc12c1cda3c4d3d2cd9a635ae9
Ashok Gelal <ashok@metageek.net>
2020-01-12 22:01:48 -0700

    Add project routes and clean it up

--- src/main/kotlin/routes.kt
+++ src/main/kotlin/routes.kt
@@ -1,15 +1,31 @@
 package dev.alpas.fireplace
 
 import dev.alpas.auth.authRoutes
+import dev.alpas.fireplace.controllers.ProjectController
 import dev.alpas.fireplace.controllers.WelcomeController
+import dev.alpas.routing.RouteGroup
 import dev.alpas.routing.Router
 
 fun Router.addRoutes() = apply {
-    webRoutes()
+    group {
+        webRoutesGroup()
+    }.middlewareGroup("web")
     authRoutes(requireEmailVerification = false)
 }
 
-private fun Router.webRoutes() {
-    get("/", WelcomeController::class).name("welcome").middlewareGroup("web")
+private fun RouteGroup.webRoutesGroup() {
+    get("/", WelcomeController::class).name("welcome")
+
+    group("projects") {
+        addProjectRoutes()
+    }.name("projects").mustBeAuthenticated()
+}
+
+private fun RouteGroup.addProjectRoutes() {
+    get("/", ProjectController::class).name("list")
+    get("/create", ProjectController::class, "create").name("create")
+    get("/<id>", ProjectController::class, "show").name("show")
+    post("/", ProjectController::class).name("store")
+    delete("/", ProjectController::class).name("delete")
 }
 
