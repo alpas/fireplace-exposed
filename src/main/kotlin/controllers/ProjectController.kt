d73244ba3f8697c551ec56bfba6957857828f3cc
Ashok Gelal <ashok@metageek.net>
2020-01-13 11:41:00 -0700

    Show a project

--- src/main/kotlin/controllers/ProjectController.kt
+++ src/main/kotlin/controllers/ProjectController.kt
@@ -1,5 +1,6 @@
 package dev.alpas.fireplace.controllers
 
+import dev.alpas.fireplace.entities.Project
 import dev.alpas.fireplace.entities.Projects
 import dev.alpas.fireplace.entities.User
 import dev.alpas.fireplace.guards.CreateProjectGuard
@@ -30,6 +31,18 @@ class ProjectController : Controller() {
             }
         }
         call.redirect().toRouteNamed("projects.list")
+    }
+
+    fun show(call: HttpCall) {
+        val projectId = call.paramAsLong("id").orAbort()
+        val userId = auth().user?.id()
+        transaction {
+            val project = Project.find {
+                (Projects.owner eq userId) and (Projects.id eq projectId)
+            }.firstOrNull().orAbort()
+
+            call.render("project_show", mapOf("project" to project))
+        }
     }
 
     fun delete(call: HttpCall) {
